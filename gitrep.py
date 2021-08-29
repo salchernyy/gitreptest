7182e61e7bc5832a4eaadfd27c06e0424b492ea1
sssalllXD <85771929+sssalllXD@users.noreply.github.com>
2021-08-29 14:12:43 -0400

    Add files via upload

---
+++ gitrep.py
@@ -0,0 +1,11 @@
+
+gitrep = input("is this from a github repository? (y/n) > ").lower()
+
+if gitrep == "y":
+    print("i agree")
+elif gitrep == "n":
+    print("you're wrong")
+else:
+    print("y/n bastard")
+
+input("")
